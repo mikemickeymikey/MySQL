bf90e74190f99601925b934e31820eefa9f1de03
Miguel Cuesta <a2106885@institutmontilivi.cat>
2023-08-16 18:54:34 +0200

    first commit

---
+++ BBDDMySQL/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace BBDDMySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDades = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDades
+            // 
+            this.dgvDades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDades.Location = new System.Drawing.Point(136, 45);
+            this.dgvDades.Name = "dgvDades";
+            this.dgvDades.RowHeadersWidth = 62;
+            this.dgvDades.RowTemplate.Height = 28;
+            this.dgvDades.Size = new System.Drawing.Size(982, 599);
+            this.dgvDades.TabIndex = 0;
+            this.dgvDades.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 757);
+            this.Controls.Add(this.dgvDades);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDades)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDades;
+    }
+}
+
---
+++ BBDDMySQL/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBDDMySQL
+{
+    public partial class Form1 : Form
+    {
+        private classicmodelsDataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ds = new classicmodelsDataSet();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.productsTableAdapter.FillByPLineMore100(ds.products, "Ships");
+        }
+
+        
+    }
+}
